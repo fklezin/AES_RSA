b1bb1766fda7fa77dd6ca9ea98ed96949f954244
fklezin <klezin.florijan95@gmail.com>
2017-01-06 12:55:29 +0100

    Working console application with example

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         try {
             AES aes = new AES("keys/AESkey.key","documents/AES/encrypted.txt","documents/AES/original.txt");
             aes.encrypt();
@@ -22,12 +21,14 @@ public class Main {
             e.printStackTrace();
         }
 
+        try {
+            RSA rsa = new RSA("keys/RSAprivate.key","keys/RSApublic.key","documents/RSA/encrypted.txt","documents/RSA/original.txt");
+            rsa.encrypt();
 
-
-       /* try {
-            new RSA("keys/private.key","keys/public.key",2048,"file.txt","document.txt").runRSA();
-        } catch (RSAException e) {
-            e.printStackTrace();
-        }*/
+            rsa = new RSA("keys/RSAprivate.key","keys/RSApublic.key","documents/RSA/decrypted.txt","documents/RSA/encrypted.txt");
+            rsa.decrypt();
+        }catch (RSAException ex){
+            ex.printStackTrace();
+        }
     }
 }
